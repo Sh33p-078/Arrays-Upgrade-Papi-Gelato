259d04426b261b596f32fc6b8a1d66b086bcdf2b
Quinten W <sh33pspammail@gmail.com>
2023-02-15 13:13:36 +0100

    Opdracht 2 afgerond

--- Opdracht 2/script.js
+++ Opdracht 2/script.js
@@ -1,9 +1,20 @@
 let names = [];
 
-for (;;) {
+while (true) {
     let name = prompt("Enter a name (or 'stop' to finish):");
     if (name === "stop") {
         break;
     }
     names.push(name);
+}
+
+if (names.length < 3) {
+    alert("Error: at least 3 names are required.");
+} else {
+    let namesList = document.getElementById("names-list");
+    for (let i = 0; i < names.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = names[i];
+        namesList.appendChild(li);
+    }
 }
