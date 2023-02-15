81e4f0a0a02bf0905d272754870e730f3a248683
Quinten W <sh33pspammail@gmail.com>
2023-02-15 13:33:27 +0100

    Opdracht 2 Goed Afgerond

--- Opdracht 2/script.js
+++ Opdracht 2/script.js
@@ -12,9 +12,19 @@ if (names.length < 3) {
     alert("Error: at least 3 names are required.");
 } else {
     let namesList = document.getElementById("names-list");
-    for (let i = 0; i < names.length; i++) {
+    for (let i = names.length - 1; i >= 0; i--) {
         let li = document.createElement("li");
         li.textContent = names[i];
         namesList.appendChild(li);
+    }
+
+    let reverseList = document.createElement("ul");
+    reverseList.id = "reverse-names-list";
+    namesList.parentNode.insertBefore(reverseList, namesList.nextSibling);
+
+    for (let i = 0; i < names.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = names[i];
+        reverseList.appendChild(li);
     }
 }
