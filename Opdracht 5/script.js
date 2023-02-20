3a68960c8277f08b91766ff3b66bad233b211a15
Quinten W <sh33pspammail@gmail.com>
2023-02-20 14:16:56 +0100

    Opdracht 5 afgerond

--- Opdracht 5/script.js
+++ Opdracht 5/script.js
@@ -1,12 +1,8 @@
 let getal = prompt("Voer een getal in:");
 let array = [];
-
-// Vul de array met getallen van 1 tot het ingevoerde getal
 for (let i = 1; i <= getal; i++) {
     array.push(i);
 }
-
-// Print de pyramide oplopend naar het scherm
 for (let i = 0; i < array.length; i++) {
     let output = "<br>";
     for (let j = 0; j <= i; j++) {
